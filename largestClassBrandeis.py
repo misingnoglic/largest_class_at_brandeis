3969d8b2ff016e99ddaf77d882a143f4738bf043
Arya Boudaie <aboudaie@brandeis.edu>
2017-01-15 14:38:49 -0500

    Did refactoring, added pickling, added a sound

--- largestClassBrandeis.py
+++ largestClassBrandeis.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 import time
+try:
+	import winsound
+	sound = True
+except:
+	sound = False
+
+import pickle
+
 
 driver = webdriver.Chrome()
-driver.get("https://brandeis.schdl.net/term/Spring_2017")
-already_visited = set([])
+link = "https://brandeis.schdl.net/term/Spring_2017"
 
-classes_section = []
-for i in range(3): # for each column (there are three)
+driver.get(link)
+already_visited = set([]) # List of links already visisted, so no duplicates
+# This is needed because lots of classes are crosslists - thankfully they have the same URL
+
+classes_section = [] # Store classes by section
+classes_total = [] # Store by total number of people in all the sections
+for i in range(3): # for each column (there are three - bad that this is hardcoded but eh)
 	time.sleep(1) # time for loading
 
 	column = driver.find_elements_by_class_name("col-sm-4")[i] # get that column
@@ -24,15 +36,40 @@ for i in range(3): # for each column (there are three)
 			if l and "course" in l and l not in already_visited:
 				driver.get(l)
 				already_visited.add(l)
-				time.sleep(.2)
+				time.sleep(.3)
 				spans = driver.find_elements_by_tag_name("span")
+				if len(spans)==0: # in case the page didn't load
+					driver.refresh()
+					time.sleep(3)
+					spans = driver.find_elements_by_tag_name("span")
+				# All of the span elements that have a section name
 				people = [x for x in spans if x.get_attribute("ng:show")=="exists(enrolled)"]
-				for p in people:
+				total_n = 0
+				current_class = driver.current_url.split("/")[-1]
+				for i,p in enumerate(people):
 					try:
-						n = int(p.text.split()[1])
-						classes_section.append( (n,driver.current_url) )
+						n = int(p.text.split()[1]) # Number of people in class
+						# Append a tuple of number of people, class name, and section
+						classes_section.append( (n,current_class,i+1) )
+						total_n+=n
 					except:
 						pass
-	driver.get("https://brandeis.schdl.net/term/Spring_2017")
-driver.close()
-print(sorted(classes_section, key=lambda x:x[0]))
+				classes_total.append( (total_n, current_class) )
+	driver.get(link)
+driver.quit()
+
+sorted_classes_section = sorted(classes_section, key=lambda x:x[0])
+print(sorted_classes_section)
+sorted_classes_total = sorted(classes_total, key=lambda x:x[0])
+if sound: winsound.MessageBeep()
+input()
+print(sorted_classes_total)
+
+with open("classes_by_section.pickle", "wb") as classes_by_section:
+	pickle.dump(sorted_classes_section, classes_by_section)
+
+
+with open("classes_total.pickle", "wb") as classes_total:
+	pickle.dump(sorted_classes_total, classes_total)
+
+
